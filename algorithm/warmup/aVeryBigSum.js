639eb89dfad5ad92112ad9fb6cbd5781c22106fc
Andy Winarko <winarcooo@gmail.com>
2019-03-11 17:49:52 +0700

    a very big sum

--- algorithm/warmup/aVeryBigSum.js
+++ algorithm/warmup/aVeryBigSum.js
@@ -30,10 +30,17 @@
 */
 
 // Complete the aVeryBigSum function below.
-function aVeryBigSum(n, ar) {
-    return arr
+function aVeryBigSum(ar) {
+    var sum = ar.reduce(add);
+    function add(accumulator, a) {
+        return accumulator + a
+    }
+    return sum
 }
 
-let result = aVeryBigSum(ar);
+(function(){
+    var arr = [1000000001,1000000002,1000000003,1000000004,1000000005]
+    console.log(aVeryBigSum(arr))
+})();
 
 
