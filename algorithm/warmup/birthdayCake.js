4c6064e4a80935afc3d7a5f0340a8af0ee281072
Andy Winarko <winarcooo@gmail.com>
2019-03-12 18:28:38 +0700

    warmup

---
+++ algorithm/warmup/birthdayCake.js
@@ -0,0 +1,14 @@
+function birthdayCake(ar) {
+    // find maximum number
+    var max = Math.max(...ar)
+    
+    // search each number in array that same number as max and save to new variable
+    var result = ar.filter(number => number == max)
+
+    // return how much inside result array after searching
+    return result.length
+}
+
+var ar = [2,7,1,5]
+var result = birthdayCake(ar)
+console.log(result)
