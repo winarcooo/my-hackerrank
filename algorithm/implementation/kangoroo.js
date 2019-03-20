09a16050b276a7a63bd2f5078a107ca0040dad7c
Andy Winarko <winarcooo@gmail.com>
2019-03-20 21:58:32 +0700

    rewrite

--- algorithm/implementation/kangoroo.js
+++ algorithm/implementation/kangoroo.js
@@ -1,27 +1,5 @@
 function kangoroo(arr) {
-    var postA = arr[0]
-    var postB = arr[2]
-
-    var jumpA = arr[1]
-    var jumpB = arr[3]
-
-    if (postA < postB) {
-        if (jumpA < jumpB) {
-            return 'NO';
-        } else if (jumpA > jumpB) {
-            return "YES";
-        } else {
-            return 'NO';
-        }
-    } else if(postB < postA) {
-        if (jumpA > jumpB) {
-            return 'NO';
-        } else if (jumpB > jumpA) {
-            return 'YES';
-        } else {
-            return 'NO';
-        }
-    }
+    
 }
 
 module.exports = kangoroo;
