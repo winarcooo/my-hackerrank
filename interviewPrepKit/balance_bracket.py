344590e46288b3d13ff2b3bf8a167ad5ad2be0dd
Andy Winarko <winarcooo@gmail.com>
2019-12-19 17:48:28 +0700

    Create balance_bracket.py

---
+++ interviewPrepKit/balance_bracket.py
@@ -0,0 +1,62 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def isPair(open, close):
+  if open == "(" and close == ")":
+    return True
+  elif open == "{" and close == "}":
+    return True
+  elif open == "[" and close == "]":
+    return True
+  return False
+
+def isOpen(param):
+  if param in ("(", "{", "["):
+    return True
+  return False
+
+def isClose(param):
+  if param in (")", "}", "]"):
+    return True
+  return False
+  
+
+# Complete the isBalanced function below.
+def isBalanced(string):
+  temporary = []
+  brackets = list(string)
+
+  for item in brackets:
+
+    if len(temporary) == 0 and isClose(brackets):
+      return "NO"
+
+    if isOpen(item):
+      temporary.append(item)
+    
+    if isClose(item):
+      prevBracket = temporary[-1]
+      if isPair(prevBracket, item):
+        temporary.pop()
+      else:
+        return "NO"
+
+        
+  print(temporary)
+  if len(temporary) == 0:
+    return "YES"
+  
+  return "NO"
+
+
+if __name__ == '__main__':
+  input0 = "{(([])[])[]]}"
+  input1 = "{[()]}"
+
+  result = isBalanced(input0)
+  print(result)
